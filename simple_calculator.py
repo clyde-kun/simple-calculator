8ce1e6017e0d4a5018db5a23a060e517cc1ac770
clyde <56524676+mis-fit@users.noreply.github.com>
2021-03-09 19:32:11 -0700

    Added option to perform another calculation
    Using the while function I programmed it to either end the program when enter was clicked and to repeat the process if continue was typed.

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,28 +1,32 @@
-operation = input('What math operation will you be using? ')
+close = 'continue'
 
-if operation == 'Addition':
-    add1 = float(input('Number '))
-    add2 = float(input('Plus '))
-    print(add1 + add2)
+while close == 'continue':
+    operation = input('What math operation will you be using? ')
+    if operation == 'Addition':
+        add1 = float(input('Number '))
+        add2 = float(input('Plus '))
+        print(add1 + add2)
 
-if operation == 'Subtraction':
-    sub1 = float(input("Number "))
-    sub2 = float(input('Subracted '))
-    print(sub1 - sub2)
+    if operation == 'Subtraction':
+        sub1 = float(input("Number "))
+        sub2 = float(input('Subracted '))
+        print(sub1 - sub2)
 
-if operation == 'Division':
-    div1 = float(input("Number "))
-    div2 = float(input("Divided by "))
-    print(div1 / div2)
+    if operation == 'Division':
+        div1 = float(input("Number "))
+        div2 = float(input("Divided by "))
+        print(div1 / div2)
 
-if operation == 'Multiplication':
-    mul1 = float(input('Number '))
-    mul2 = float(input("Multiplied by "))
-    print(mul1 * mul2)
+    if operation == 'Multiplication':
+        mul1 = float(input('Number '))
+        mul2 = float(input("Multiplied by "))
+        print(mul1 * mul2)
 
-if operation == 'Exponentation':
-    exp1 = float(input("Number "))
-    exp2 = float(input("Raised to "))
-    print(exp1 ** exp2)
+    if operation == 'Exponentation':
+        exp1 = float(input("Number "))
+        exp2 = float(input("Raised to "))
+        print(exp1 ** exp2)
 
-close = input("press enter to end program")
+    close = input("press enter to end program or type continue to perform another calculation ")
+
+
