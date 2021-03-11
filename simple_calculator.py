d0210c97b5c888ceec62731872abb0f4bdbbd1c3
clyde <56524676+mis-fit@users.noreply.github.com>
2021-03-10 17:16:04 -0700

    Added ability to use more than 2 numbers

--- simple_calculator.py
+++ simple_calculator.py
@@ -3,28 +3,109 @@ close = 'continue'
 while close == 'continue':
     operation = input('What math operation will you be using? ')
     if operation == 'Addition':
-        add1 = float(input('Number '))
-        add2 = float(input('Plus '))
-        print(add1 + add2)
+        numbers = input("How many numbers will you be using? ")
+        if numbers == "2":
+            num1 = float(input('Number '))
+            num2 = float(input('Plus '))
+            print(num1 + num2)
+        if numbers == "3": 
+            num1 = float(input('Number '))
+            num2 = float(input('Plus '))
+            num3 = float(input('Plus '))
+            print(num1 + num2 + num3)
+        if numbers == "4": 
+            num1 = float(input('Number '))
+            num2 = float(input('Plus '))
+            num3 = float(input('Plus '))
+            num4 = float(input('Plus '))
+            print(num1 + num2 + num3 + num4)
+        if numbers == "5": 
+            num1 = float(input('Number '))
+            num2 = float(input('Plus '))
+            num3 = float(input('Plus '))
+            num4 = float(input('Plus '))
+            num5 = float(input('Plus '))
+            print(num1 + num2 + num3 + num4 + num5)
 
     if operation == 'Subtraction':
-        sub1 = float(input("Number "))
-        sub2 = float(input('Subracted '))
-        print(sub1 - sub2)
-
+        numbers = input("How many numbers will you be using? ")
+        if numbers == "2":
+            num1 = float(input("Number "))
+            num2 = float(input('Subracted '))
+            print(num1 - num2)
+        if numbers == "3":
+            num1 = float(input("Number "))
+            num2 = float(input('Subracted '))
+            num3 = float(input('Subtracted by '))
+            print(num1 - num2 - num3)
+        if numbers == "4": 
+            num1 = float(input('Number '))
+            num2 = float(input('Subracted '))
+            num3 = float(input('Subtracted by '))
+            num4 = float(input('Subtracted by '))
+            print(num1 - num2 - num3 - num4)
+        if numbers == "5": 
+            num1 = float(input('Number '))
+            num2 = float(input('Subracted '))
+            num3 = float(input('Subtracted by '))
+            num4 = float(input('Subtracted by '))
+            num5 = float(input('Subtracted by '))
+            print(num1 - num2 - num3 - num4 - num5)
+        
     if operation == 'Division':
-        div1 = float(input("Number "))
-        div2 = float(input("Divided by "))
-        print(div1 / div2)
+        numbers = input("How many numbers will you be using? ")
+        if numbers == "2":
+            num1 = float(input("Number "))
+            num2 = float(input("Divided by "))
+            print(num1 / num2)
+        if numbers == "3":
+            num1 = float(input("Number "))
+            num2 = float(input('Divided by '))
+            num3 = float(input('Divided by '))
+            print(num1 / num2 / num3)
+        if numbers == "4": 
+            num1 = float(input('Number '))
+            num2 = float(input('Divided by '))
+            num3 = float(input('Divided by '))
+            num4 = float(input('Divided by '))
+            print(num1 / num2 / num3 / num4)
+        if numbers == "5": 
+            num1 = float(input('Number '))
+            num2 = float(input('Divided by '))
+            num3 = float(input('Divided by '))
+            num4 = float(input('Divided by '))
+            num5 = float(input('Divided by '))
+            print(num1 / num2 / num3 / num4 / num5)
 
     if operation == 'Multiplication':
-        mul1 = float(input('Number '))
-        mul2 = float(input("Multiplied by "))
-        print(mul1 * mul2)
+        numbers = input("How many numbers will you be using? ")
+        if numbers == "2":
+            num1 = float(input('Number '))
+            num2 = float(input("Multiplied by "))
+            print(num1 * num2)
+        if numbers == "3":
+            num1 = float(input("Number "))
+            num2 = float(input('Multiplied by '))
+            num3 = float(input('Times '))
+            print(num1 * num2 * num3)
+        if numbers == "4": 
+            num1 = float(input('Number '))
+            num2 = float(input('Multiplied by '))
+            num3 = float(input('Multiplied by '))
+            num4 = float(input('Multiplied by '))
+            print(num1 * num2 * num3 * num4)
+        if numbers == "5": 
+            num1 = float(input('Number '))
+            num2 = float(input('Multiplied by '))
+            num3 = float(input('Multiplied by '))
+            num4 = float(input('Multiplied by '))
+            num5 = float(input('Multiplied by '))
+            print(num1 * num2 * num3 * num4 * num5)
 
     if operation == 'Exponentation':
-        exp1 = float(input("Number "))
-        exp2 = float(input("Raised to "))
+        numbers = input("How many numbers will you be using? ")
+        num1 = float(input("Number "))
+        num2 = float(input("Raised to "))
         print(exp1 ** exp2)
 
     close = input("press enter to end program or type continue to perform another calculation ")
